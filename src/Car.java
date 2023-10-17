f71fb8c173ff9184897f4a93da3734c072d5e171
Victoriia Pliasun <vipliasun@gmail.com>
2023-10-17 14:28:19 +0300

    HW 3

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    int price = 300;
+    String type;
+
+    public int setPrice(int newPrice){
+        this.price = newPrice;
+        return price;
+    }
+    public String setType (String newType){
+        this.type = newType;
+        return type;
+    }
+
+}
+
